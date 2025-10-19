5c478796b9875d8d70fbbc7e3cf8eb51860af630
LorendosCSharp <124144453+LorendosCSharp@users.noreply.github.com>
2025-10-19 04:32:31 +0200

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
-
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   base: '/ImagesToSatisfactoryBlueprints/',
   plugins: [vue()],
+  optimizeDeps: {
+    include: ['@etothepii/satisfactory-file-parser']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/@etothepii/, /node_modules/]
+    }
+  }
 })
